4812c9b4da8d4e4adafeaeb8e1066f90e458b971
mbiftu <moa.biftu@yahoo.com>
2016-09-18 18:15:23 -0400

    plot2

---
+++ plot2.R
@@ -0,0 +1,11 @@
+base <- "C:/Users/Moa/Desktop/coursera/expdata/household_power_consumption.txt"
+org <- read.table(base, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subsetorg <- org[org$Date %in% c("1/2/2007","2/2/2007") ,]
+
+##subsetting the Data
+datetime <- strptime(paste(subsetorg$Date, subsetorg$Time, sep=" "), "%d/%m/%Y %H:%M:%S")  
+ globalActivePower <- as.numeric(subsetorg$Global_active_power) 
+ png("plot2.png", width=480, height=480) 
+ plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)") 
+ dev.off() 
+
